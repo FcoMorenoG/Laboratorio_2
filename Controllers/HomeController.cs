19387d1592357a5161fea444f3d771e8e1a1741e
Francisco Moreno <fransalvadormoreno@gmail.com>
2024-10-10 23:35:30 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Acerca de.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contactame.";
+
+            return View();
+        }
+        public ActionResult Formacion()
+        {
+            ViewBag.Message = "Pagina de mi formacion academica.";
+
+            return View();
+        }
+        public ActionResult Experiencia()
+        {
+            ViewBag.Message = "Pagina de mi experiencia.";
+
+            return View();
+        }
+        public ActionResult Habilidades()
+        {
+            ViewBag.Message = "Pagina de mis habilidades.";
+
+            return View();
+        }
+        public ActionResult Repositos()
+        {
+            ViewBag.Message = "Pagina de mis Repositos.";
+
+            return View();
+        }
+    }
+}
